07e05e75015e05131e45b342b058b2cd67f0af69
AdelakinFaruq <faruqadelakin06@gmail.com>
2026-09-20 01:36:59 +0100

    Fix Django project settings reference

--- temp_project/temp_project/settings.py
+++ temp_project/temp_project/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "temp_project.urls"
+ROOT_URLCONF = "myproject.urls"
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "temp_project.wsgi.application"
+WSGI_APPLICATION = "myproject.wsgi.application"
 
 
 # Database
